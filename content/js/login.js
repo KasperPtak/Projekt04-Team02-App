c1026697a9b8a01f0a12ba275fe7e3ed20476745
KasperPtak <kasperptak2@gmail.com>
2021-09-24 18:45:29 +0200

    lavet js til login og register page
    Co-Authored-By: mariasolsikke <82049753+mariasolsikke@users.noreply.github.com>

---
+++ content/js/login.js
@@ -0,0 +1,88 @@
+
+// var er es6 
+// const/let er es7 
+
+var emails = [
+    "Mariamail@gmail.com",
+    "test",
+    "Trinemail@gmail.com",
+    "Nannamail@gmail.com"
+]
+
+var loginEmail = document.getElementById('loginEmail');
+var loginPassword = document.getElementById('loginPassword');
+
+function checklogin() {
+
+    // tjekker om der er skrevet noget i input felterne
+    if (loginEmail.value.length == 0 && loginPassword.value.length == 0) {
+        alert("skriv noget pls")
+
+    }
+
+    // tjekker om der er skrevet det rigtige password
+        else if (loginPassword.value == "password") {
+
+            // tjekker om der er skrevet den rigtige email
+            switch (loginEmail.value) {
+
+                case emails[0]:
+                    alert(emails[0] + " er nu logget ind")
+                    window.location = "../views/dineBegivenheder.html";
+                    break;
+                case emails[1]:
+                    alert(emails[1] + " er nu logget ind")
+                    window.location = "../views/dineBegivenheder.html";
+                    break;
+                case emails[2]:
+                    alert(emails[2] + " er nu logget ind")
+                    window.location = "../views/dineBegivenheder.html";
+                    break;
+                case emails[3]:
+                    alert(emails[3] + " er nu logget ind")
+                    window.location = "../views/dineBegivenheder.html";
+                    break;
+
+                default: alert("Din email er forkert");
+                    // Hvis emailen er skrevet forkert
+                    break;
+            }
+
+        } else {
+            // Hvis password er skrevet forkert
+            alert("Dit password er forkert")
+
+        }
+
+}
+
+
+
+
+
+    // href="../views/registrer.html"
+
+
+
+    // if (loginEmail.value.length == 0 && loginPassword.value.length == 0) {
+    //     alert("skriv noget pls")
+
+    // }
+    // else {
+
+        // if (loginEmail.value == emails[0]) {
+        //     console.log(emails[0] + " har logget ind");
+
+        // }
+        // if (loginEmail.value == emails[1]) {
+        //     console.log(emails[1] + " har logget ind");
+
+        // }
+        // if (loginEmail.value == emails[3]) {
+        //     console.log(emails[3] + " har logget ind");
+
+        // }
+        // if (loginEmail.value == emails[4]) {
+        //     console.log(emails[4] + " har logget ind");
+
+        // }
--- content/js/registrer.js
+++ content/js/registrer.js
@@ -4,13 +4,19 @@ var email = document.getElementById('email-input')
 var adgangskode = document.getElementById('adgangskodeInput')
 var bekraeft = document.getElementById('bekraeft-adgangskode-input')
 var stjerne = document.getElementById('stjerne')
+var checkbox = document.getElementById('checkbox')
 
 function opret(){
     
+    console.log(checkbox.checked)
     
-    if( adgangskode.value.length == 0 && email.value.length == 0 && bekraeft.value.length == 0){
+    if( adgangskode.value.length == 0 && email.value.length == 0 && bekraeft.value.length == 0 && checkbox.checked == false){
         console.log("adganskode og email og bekræft er tom");
         stjerne.style.display = "block"
+    }
+    else {
+        stjerne.style.display = "none"
+        // window.location =
     }
 }
 
